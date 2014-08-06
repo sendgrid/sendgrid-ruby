a3efde19ba6ebb4d09d1aedcf6bca6ef85a0d310
Eddie Zaneski <eddiezane@gmail.com>
2014-08-06 16:53:31 -0400

    Clean up post payload

--- lib/sendgrid-ruby.rb
+++ lib/sendgrid-ruby.rb
@@ -17,8 +17,9 @@ module SendGrid
     end
 
     def send(mail)
-      # TODO: This feels off possibly
-      @conn[@endpoint].post(mail.to_h.merge({api_user: @api_user, api_key: @api_key}), {user_agent: 'sendgrid/' + SendGrid::VERSION + ';ruby'}) do |response, request, result|
+      payload = mail.to_h.merge({api_user: @api_user, api_key: @api_key})
+      @conn[@endpoint].post(payload, {user_agent: 'sendgrid/' + SendGrid::VERSION + ';ruby'}) do |response, request, result|
+        p request
         case response.code.to_s
         when /2\d\d/
           response
