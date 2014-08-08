9cd3333d3ac2e4ab4f4adf2011fa9ec0485f1229
Eddie Zaneski <eddiezane@gmail.com>
2014-08-08 15:25:50 -0400

    Remove test file

--- test.rb
+++
@@ -1,7 +0,0 @@
-require './lib/sendgrid-ruby.rb'
-
-client = SendGrid::Client.new ENV['SENDGRID_USERNAME'], ENV['SENDGRID_PASSWORD']
-email = SendGrid::Mail.new(from: 'root@mail.doesnotscale.com', subject: 'Test', text: 'Body of sexy')
-email.add_to 'eddiezane@sendgrid.com'
-email.add_to 'rbin@sendgrid.com'
-puts client.send(email)
