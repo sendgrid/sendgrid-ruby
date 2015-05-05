82b5f207ff35a488270a752fec82708a28b9d823
Eddie Zaneski <eddiezane@gmail.com>
2015-05-05 00:03:38 -0400

    Add and rework address methods

--- lib/sendgrid/mail.rb
+++ lib/sendgrid/mail.rb
@@ -8,16 +8,84 @@ module SendGrid
 
     def initialize(params = {})
       params.each do |k, v|
-        instance_variable_set("@#{k}", v) unless v.nil?
+        send(:"#{k}=", v) unless v.nil?
       end
-      @headers     ||= {}
-      @attachments ||= []
-      @smtpapi     ||= Smtpapi::Header.new
       yield self if block_given?
     end
 
-    def add_to(to_email)
-      @smtpapi.add_to to_email
+    def add_to(email, name = nil)
+      if email.is_a?(Array)
+        to.concat(email)
+      else
+        to << email
+      end
+      add_to_name(name) if name
+    end
+
+    def to
+      @to ||= []
+    end
+
+    def to_name
+      @to_name ||= []
+    end
+
+    def add_to_name(name)
+      if name.is_a?(Array)
+        to_name.concat(name)
+      else
+        to_name << name
+      end
+    end
+
+    def cc
+      @cc ||= []
+    end
+
+    def cc_name
+      @cc_name ||= []
+    end
+
+    def add_cc(email, name = nil)
+      if email.is_a?(Array)
+        cc.concat(email)
+      else
+        cc << email
+      end
+      add_cc_name(name) if name
+    end
+
+    def add_cc_name(name)
+      if name.is_a?(Array)
+        cc_name.concat(name)
+      else
+        cc_name << name
+      end
+    end
+
+    def bcc
+      @bcc ||= []
+    end
+
+    def bcc_name
+      @bcc_name ||= []
+    end
+
+    def add_bcc(email, name = nil)
+      if email.is_a?(Array)
+        bcc.concat(email)
+      else
+        bcc << email
+      end
+      add_bcc_name(name)
+    end
+
+    def add_bcc_name(name)
+      if name.is_a?(Array)
+        bcc_name.concat(name)
+      else
+        bcc_name << name
+      end
     end
 
     def add_attachment(path, name = nil)
@@ -26,35 +94,53 @@ module SendGrid
       @attachments << {file: file, name: name}
     end
 
+    def headers
+      @headers ||= {}
+    end
+
+    def attachments
+      @attachments ||= []
+    end
+
+    def smtpapi
+      @smtpapi ||= Smtpapi::Header.new
+    end
+
     def to_h
       payload = {
-        :from        => @from,
-        :fromname    => @from_name,
-        :subject     => @subject,
-        :to          => @to,
-        :toname      => @to_name,
-        :date        => @date,
-        :replyto     => @reply_to,
-        :cc          => @cc,
-        :bcc         => @bcc,
-        :text        => @text,
-        :html        => @html,
-        :'x-smtpapi' => @smtpapi.to_json,
-        :files       => ({} unless @attachments.empty?)
+        :from        => from,
+        :fromname    => from_name,
+        :subject     => subject,
+        :to          => to,
+        :toname      => to_name,
+        :date        => date,
+        :replyto     => reply_to,
+        :cc          => cc,
+        :bcc         => bcc,
+        :text        => text,
+        :html        => html,
+        :'x-smtpapi' => smtpapi.to_json,
+        :files       => ({} unless attachments.empty?)
       }.reject {|k,v| v.nil?}
 
-      # smtpapi fixer
-      if @to.nil? and not @smtpapi.to.empty?
+      # smtpapi bandaid
+      if to.nil? && !smtpapi.to.empty?
         payload[:to] = payload[:from]
       end
 
-      unless @attachments.empty?
-        @attachments.each do |file|
-          payload[:files][file[:name]] = file[:file]
-        end
-      end
+      payload[:to] = payload[:from] if no_adressee?
 
+      return if  @attachments.empty?
+      @attachments.each do |file|
+        payload[:files][file[:name]] = file[:file]
+      end
       payload
+    end
+
+
+    private 
+    def smtpapi_bandaid
+
     end
   end
 end
