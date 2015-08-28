89a5c8bac4c781d60c69d0b1e907785b7d178ba9
Jake Yesbeck <jake@apartmentlist.com>
2015-08-28 14:10:23 -0700

    Recipient introduced
    To help encapsulate the substitution logic for templates and how they
      correlate with email recipients, this model has been introduced.
    
    The benefit is now something like this is possible:
    
    ```ruby
    template = Template.new(TEMPLATE_ID)
    
    users = User.find(['firstuser@gmail.com', 'seconduser@gmail.com'])
    
    users.each do |user|
      recipient = Recipient.new(user)
      recipient.add_substitution(:name, user.name)
      recipient.add_substitution(:location, user.location)
    
      template.add_recipient(recipient)
    end
    ```
    
    The next step will be to integrate the `Recipient` and `Template`
      with `Mail`.

--- lib/sendgrid/template.rb
+++ lib/sendgrid/template.rb
@@ -1,11 +1,17 @@
 require 'smtpapi'
+require_relative './recipient'
 
 module SendGrid
   class Template
-    attr_reader :id
+    attr_reader :id, :recipients
 
     def initialize(id)
       @id = id
+      @recipients = []
+    end
+
+    def add_recipient(recipient)
+      recipients << recipient
     end
 
     def add_to_smtpapi(smtpapi)
@@ -14,6 +20,7 @@ module SendGrid
       smtpapi.tap do |api|
         api.add_filter(:template, :enabled, 1)
         api.add_filter(:template, :id, id)
+        recipients.each { |r| r.add_to_smtpapi(smtpapi) }
       end
     end
   end
