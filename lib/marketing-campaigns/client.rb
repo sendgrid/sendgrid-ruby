40670e4c313869c15bc72339b9730af86b00948d
ehudc <s.ehud.cohen@gmail.com>
2015-12-02 16:57:29 -0700

    XFLN-2 marketing campaigns Create and Get actions uses only api keys for security + simplicity returns all json responses, MC API handles fail cases

---
+++ lib/marketing-campaigns/client.rb
@@ -0,0 +1,74 @@
+require 'active_support/all'
+require 'faraday'
+require 'pry'
+
+module MarketingCampaigns
+  class Client
+    attr_accessor :api_user, :api_key, :protocol, :host, :port, :url, :endpoint,
+                  :user_agent, :template
+    attr_writer :adapter, :conn, :raise_exceptions
+
+    def initialize(params = {})
+      self.api_key          = params.fetch(:api_key, nil)
+      self.protocol         = params.fetch(:protocol, 'https')
+      self.host             = params.fetch(:host, 'api.sendgrid.com')
+      self.port             = params.fetch(:port, nil)
+      self.url              = params.fetch(:url, protocol + '://' + host + (port ? ":#{port}" : ''))
+      self.endpoint         = params.fetch(:endpoint, '/v3/campaigns')
+      self.adapter          = params.fetch(:adapter, adapter)
+      self.conn             = params.fetch(:conn, conn)
+      self.user_agent       = params.fetch(:user_agent, "sendgrid/#{SendGrid::VERSION};ruby")
+      yield self if block_given?
+    end
+
+    def create(campaign)
+      res = conn.post do |req|
+        payload = campaign.to_json
+        build(req)
+        req.url(endpoint)
+        req.body = payload
+      end
+      output(res)
+    end
+
+    def get(id = '')
+      res = conn.get do |req|
+        build(req)
+        if id.present?
+          req.url(endpoint + '/' + id.to_s)
+        else
+          req.url(endpoint)
+        end
+      end
+      output(res)
+    end
+
+    def conn
+      @conn ||= Faraday.new(url: url) do |conn|
+        conn.request :multipart
+        conn.request :url_encoded
+        conn.adapter adapter
+      end
+    end
+
+    def build(request)
+      request.headers['Authorization'] = "Bearer #{api_key}"
+      # GZIP encoding issue
+      # https://github.com/lostisland/faraday/issues/120
+      request.headers['accept-encoding'] = "none"
+    end
+
+    def adapter
+      @adapter ||= Faraday.default_adapter
+    end
+
+    def output(res)
+      # marketing campaigns API returns multiple statuses, including failed with reasons
+      SendGrid::Response.new(code: res.status, headers: res.headers, body: res.body)
+    end
+
+    def raise_exceptions?
+      @raise_exceptions
+    end
+  end
+end
---
+++ spec/lib/marketing-campaigns/client_spec.rb
@@ -0,0 +1,66 @@
+require 'json'
+require 'spec_helper'
+require 'webmock/rspec'
+
+describe 'MarketingCampaigns::Client' do
+  it 'should accept an api key' do
+    expect(MarketingCampaigns::Client.new(api_key: 'sendgrid_123')).to be_an_instance_of(MarketingCampaigns::Client)
+  end
+
+  it 'should build the default url' do
+    expect(MarketingCampaigns::Client.new.url).to eq('https://api.sendgrid.com')
+  end
+
+  it 'should build a custom url' do
+    expect(MarketingCampaigns::Client.new(port: 3000, host: 'foo.sendgrid.com', protocol: 'tacos').url).to eq('tacos://foo.sendgrid.com:3000')
+  end
+
+  it 'should use the default endpoint' do
+    expect(MarketingCampaigns::Client.new.endpoint).to eq('/v3/campaigns')
+  end
+
+  it 'accepts a block' do
+    expect { |b| MarketingCampaigns::Client.new(&b) }.to yield_control
+  end
+
+  it 'should have an auth header when using an api key' do
+    stub_request(:any, 'https://api.sendgrid.com/v3/campaigns')
+        .to_return(body: {message: 'success'}.to_json, status: 200, headers: {'X-TEST' => 'yes'})
+
+    client = MarketingCampaigns::Client.new(api_key: 'abc123')
+    mail = MarketingCampaigns::Campaign.new(title: 'test title')
+
+    client.create(mail)
+
+    expect(WebMock).to have_requested(:post, 'https://api.sendgrid.com/v3/campaigns')
+                           .with(headers: {'Authorization' => 'Bearer abc123'})
+  end
+
+  describe ':create' do
+    it 'should return campaign data on success' do
+      stub_request(:post, 'https://api.sendgrid.com/v3/campaigns')
+          .to_return(body: {id: 123, title: "Untitled"}.to_json, status: 201, headers: {'X-TEST' => 'yes'})
+
+      client = MarketingCampaigns::Client.new(api_key: 'abc123')
+      mail = MarketingCampaigns::Campaign.new(title: 'test title')
+      res = client.create(mail)
+      expect(res.code).to eq(201)
+      expect(res.body['id']).to eq(123)
+      expect(res.body['title']).to eq('Untitled')
+    end
+  end
+
+  describe ':get' do
+    it 'should return campaign data on success' do
+      stub_request(:get, 'https://api.sendgrid.com/v3/campaigns/123')
+          .to_return(body: {id: 123, title: "Untitled"}.to_json, status: 200, headers: {'X-TEST' => 'yes'})
+
+      client = MarketingCampaigns::Client.new(api_key: 'abc123')
+      res = client.get(123)
+      expect(res.code).to eq(200)
+      expect(res.body['id']).to eq(123)
+      expect(res.body['title']).to eq('Untitled')
+    end
+  end
+
+end
