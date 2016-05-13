882ac6f07bf084138993c7a28c2bc336bf576b50
Matthew Gray <matthew@touchofmodern.com>
2016-05-13 13:52:22 -0700

    update keyword arg for endpoint

--- lib/sendgrid/scheduled_send.rb
+++ lib/sendgrid/scheduled_send.rb
@@ -13,24 +13,24 @@ module SendGrid
     end
     
     def generate_batch_id
-      client.post(url: "/v3/mail/batch").body["batch_id"]
+      client.post(endpoint: "/v3/mail/batch").body["batch_id"]
     end
     
     def validate_batch_id(batch_id)
-      client.get(url: "/v3/mail/batch/#{batch_id}").body["batch_id"]
+      client.get(endpoint: "/v3/mail/batch/#{batch_id}").body["batch_id"]
     end
     
     def scheduled_send(batch_id)
-      client.get(url: "/v3/user/scheduled_sends/#{batch_id}").body
+      client.get(endpoint: "/v3/user/scheduled_sends/#{batch_id}").body
     end
     
     def scheduled_sends
-      client.get(url: "/v3/user/scheduled_sends").body
+      client.get(endpoint: "/v3/user/scheduled_sends").body
     end
     
     def cancel_scheduled_send(batch_id)
       if scheduled_send(batch_id).empty?
-        client.post(url: "/v3/user/scheduled_sends", 
+        client.post(endpoint: "/v3/user/scheduled_sends", 
                     payload: {
                       batch_id: batch_id,
                       status: "cancel"
@@ -42,7 +42,7 @@ module SendGrid
     
     def pause_scheduled_send(batch_id)
       if scheduled_send(batch_id).empty?
-        client.post(url: "/v3/user/scheduled_sends",
+        client.post(endpoint: "/v3/user/scheduled_sends",
                     payload: { 
                       batch_id: batch_id, 
                       status: "pause"
@@ -53,7 +53,7 @@ module SendGrid
     end
     
     def update_scheduled_send(batch_id, status)
-      client.patch(url: "/v3/user/scheduled_sends/#{batch_id}",
+      client.patch(endpoint: "/v3/user/scheduled_sends/#{batch_id}",
                    payload: { status: status })
     end
     
