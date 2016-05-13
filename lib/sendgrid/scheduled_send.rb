f2311748ea05594ce5d060ff975c35699b18b53b
Matthew Gray <matthew@touchofmodern.com>
2016-05-13 11:18:12 -0700

    auto try update scheduled send status; use client from SendGrid::Client.client

--- lib/sendgrid/scheduled_send.rb
+++ lib/sendgrid/scheduled_send.rb
@@ -3,16 +3,8 @@ require 'faraday'
 module SendGrid
   class ScheduledSend
     class << self
-      def client=(client)
-        fail client_not_found_exception unless client.is_a?(SendGrid::Client)
-        fail basic_auth_exception if client.api_user.nil? || client.api_key.nil?
-        client.conn.basic_auth(client.api_user, client.api_key)
-        @client = client
-      end
-      
       def client
-        fail client_not_found_exception unless @client.is_a?(SendGrid::Client)
-        @client
+        SendGrid::Client.client
       end
       
       def generate_batch_id
@@ -23,39 +15,41 @@ module SendGrid
         client.get(url: "/v3/mail/batch/#{batch_id}").body["batch_id"]
       end
       
+      def scheduled_send(batch_id)
+        client.get(url: "/v3/user/scheduled_sends/#{batch_id}").body
+      end
+      
       def scheduled_sends
         client.get(url: "/v3/user/scheduled_sends").body
       end
       
       def cancel_scheduled_send(batch_id)
-        client.post(url: "/v3/user/scheduled_sends", 
-                    payload: {
-                      batch_id: batch_id,
-                      status: "cancel"
-                    })
+        if scheduled_send(batch_id).empty?
+          client.post(url: "/v3/user/scheduled_sends", 
+                      payload: {
+                        batch_id: batch_id,
+                        status: "cancel"
+                      })
+        else
+          update_scheduled_send(batch_id, "cancel")
+        end
       end
       
       def pause_scheduled_send(batch_id)
-        client.post(url: "/v3/user/scheduled_sends",
-                    payload: { 
-                      batch_id: batch_id, 
-                      status: "pause"
-                    })
+        if scheduled_send(batch_id).empty?
+          client.post(url: "/v3/user/scheduled_sends",
+                      payload: { 
+                        batch_id: batch_id, 
+                        status: "pause"
+                      })
+        else
+          update_scheduled_send(batch_id, "pause")
+        end
       end
       
       def update_scheduled_send(batch_id, status)
         client.patch(url: "/v3/user/scheduled_sends/#{batch_id}",
                      payload: { status: status })
-      end
-      
-    private
-    
-      def client_not_found_exception
-        @_client_not_found_exception ||= SendGrid::Exception.new("SendGrid::Client not found. Please set SendGrid::ScheduledSend.client to an instance of SendGrid::Client")
-      end
-      
-      def basic_auth_exception
-        @_basic_aith_exception ||= SendGrid::Exception.new("You must authorize the client with your username and password. API tokens are not supported at this time.")
       end
     end
   end
