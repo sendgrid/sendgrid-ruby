8a7f8f6445eda37a1bba48b4d06fa7206c1809c2
awwa500@gmail.com <awwa500@gmail.com>
2016-10-25 00:46:40 +0900

    Add inbound parser

---
+++ lib/sendgrid/helpers/inbound/send.rb
@@ -0,0 +1,26 @@
+# A module for sending test SendGrid Inbound Parse messages
+# Usage: ruby ./send.rb [path to file containing test data]
+require 'ruby_http_client'
+require 'yaml'
+require 'optparse'
+
+OPTS = {}
+opt = OptionParser.new
+opt.on('--host=HOST') {|v| OPTS[:host] = v}
+argv = opt.parse!(ARGV)
+config = YAML.load_file(File.dirname(__FILE__) + '/config.yml')
+host = OPTS[:host] || config['host']
+client = SendGrid::Client.new(host: host)
+File.open(argv[0]) do |file|
+  data = file.read
+  headers = {
+    'User-Agent' => 'SendGrid-Test',
+    'Content-Type' => 'multipart/form-data; boundary=xYzZY'
+  }
+  response = client.post(
+    request_body: data, request_headers: headers
+  )
+  puts response.status_code
+  puts response.body
+  puts response.headers
+end
