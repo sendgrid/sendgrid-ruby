sendgrid/sendgrid-ruby

commit bd72df026fb5fd4d6941bd249feb0f38b458b35e

Updated sinatra version to 2.0
Fixes bundler dependency issues with rails >5.0 and rack 2.0.

sendgrid-ruby.gemspec
@@ -17,7 +17,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(/^(test|spec|features)/)
   spec.require_paths = ['lib']
   spec.add_dependency 'ruby_http_client', '~> 3.0'
-  spec.add_dependency 'sinatra', '~> 1.4.7'
+  spec.add_dependency 'sinatra', '2.0.0.rc2'
   spec.add_development_dependency 'rake', '~> 0'
   spec.add_development_dependency 'rspec'
   spec.add_development_dependency 'pry'

commit dd541fdc1cd18097fa85d7b57225a4c087025c4d

Relax Sinatra dependency
Fixes breaking change introduced in bd72df0
(https://github.com/sendgrid/sendgrid-ruby/pull/160), and also fixes the
issue reported in https://github.com/sendgrid/sendgrid-ruby/issues/159.

sendgrid-ruby.gemspec
@@ -17,7 +17,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(/^(test|spec|features)/)
   spec.require_paths = ['lib']
   spec.add_dependency 'ruby_http_client', '~> 3.0'
-  spec.add_dependency 'sinatra', '2.0.0.rc2'
+  spec.add_dependency 'sinatra', '>= 1.4.7', '< 3'
   spec.add_development_dependency 'rake', '~> 0'
   spec.add_development_dependency 'rspec'
   spec.add_development_dependency 'pry'