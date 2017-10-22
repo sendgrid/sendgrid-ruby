59c553d19c056b44970a8d4b464d92400e3ad9c8
Adam Jones <ajones@Nadias-iMac.local>
2017-10-21 18:53:22 -0600

    Avoid messing with encodings for(detectable) binary data content.

--- lib/sendgrid/helpers/mail/attachment.rb
+++ lib/sendgrid/helpers/mail/attachment.rb
@@ -71,10 +71,15 @@ module SendGrid
     private
 
     def encode(io)
+      str = io.read
       # Since the API expects UTF-8, we need to ensure that we're
       # converting other formats to it so (byte-wise) Base64 encoding
       # will come through properly on the other side.
-      str = io.read.encode('UTF-8')
+      #
+      # Not much to be done to try to handle encoding for files opened
+      # in binary mode, but at least we can faithfully convey the
+      # bytes.
+      str = str.encode('UTF-8') unless io.respond_to?(:binmode?) && io.binmode?
       Base64.encode64 str
     end
   end
