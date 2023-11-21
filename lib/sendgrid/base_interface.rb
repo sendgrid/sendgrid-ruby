ffe788e7cf5cafc246247b757f022edf940e2b82
manisha1997 <singhmanisha.250025@gmail.com>
2023-11-21 12:28:10 +0530

    feat: geolocation setter in sendgrid-ruby for GDPR compliance

--- lib/sendgrid/base_interface.rb
+++ lib/sendgrid/base_interface.rb
@@ -16,7 +16,7 @@ class BaseInterface
   #                              in the "On-Behalf-Of" header
   #   - +http_options+ -> http options that you want to be globally applied to each request
   #
-  def initialize(auth:, host: , region: nil, request_headers: nil, version: nil, impersonate_subuser: nil, http_options: {})
+  def initialize(auth:, host:, region: nil, request_headers: nil, version: nil, impersonate_subuser: nil, http_options: {})
     @auth = auth
     if host.nil? && region.nil?
       @host = "https://api.sendgrid.com"
